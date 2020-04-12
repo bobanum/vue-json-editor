e149748a04d144309564a5abfa76efffe25dbfa6
bobanum <mboudrea@cstj.qc.ca>
2020-04-12 15:25:38 -0400

    base interface

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<header><h1>Header</h1></header>
+		<main-nav><li><a href="#">Extra</a></li></main-nav>
+		<div class="body">Contents</div>
+		<footer>Footer</footer>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MainNav from "./components/MainNav";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {
+		MainNav,
+	}
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+:root {
+	--hue: 30;
+	--sat: 60%;
 }
+html {
+	height: 100%;
+}
+body {
+	height: 100%;
+	margin: 0;
+	background-color: hsl(var(--hue), var(--sat), 20%);
+	color: hsl(var(--hue), var(--sat), 95%);
+}
+#app {
+	min-height: 100%;
+	display: flex;
+	flex-direction: column;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	& > * {
+		padding: .5em 1em;
+	}
+	& > header {
+		background-color: hsl(var(--hue), var(--sat), 15%);
+	}
+	& > nav {
+		background-color: hsl(var(--hue), var(--sat), 10%);
+	}
+	& > .body {
+		flex: 1 0 auto;
+	}
+	& > footer {
+		background-color: hsl(var(--hue), var(--sat), 10%);
+	}
+}
+
 </style>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,42 @@
+<template>
+<nav class="main">
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="#">Link 1</a></li>
+        <li><a href="#">Link 2</a></li>
+        <li><a href="#">Link 3</a></li>
+        <slot></slot>
+    </ul>
+</nav>
+</template>
+    
+<style lang="scss">
+nav.main {
+    ul {
+        margin: 0;
+        padding: 0;
+        list-style-type: none;
+    }
+    & > ul {
+        display: flex;
+    }
+    a {
+        color: inherit;
+        text-decoration: none;
+        padding: 0 1em;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        &:hover {
+            background-color: hsl(var(--hue), var(--sat), 30%);
+        }
+    }
+}
+</style>
+
+<script>
+export default {
+    
+}
+</script>
